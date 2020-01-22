986103c1bcb67f42af6b21534aa8a7d1d3800a0b
johnfruscella <johnfruscella@gmail.com>
2020-01-22 17:19:40 -0500

    covered material in 0404,0405, and 0406. Added variable port functionality

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 MOSH Node Tutorial
 
 https://drive.google.com/drive/folders/1oYOao4-S1eM6HHUnSRwb4BmqFiLo9nL3
+
+To change port:
+Because bash is linux use : export PORT=5000 
+if using Windows cmd, then replace "export" with "set"
+https://stackoverflow.com/questions/13333221/how-to-change-value-of-process-env-port-in-node-js
--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send(`What up fool`);
+    res.send(`Welcome!`);
 
 });
 
-app.listen(3000, () => {
-    console.log(`listening on port 3000`);
+app.get('/api/courses', (req,res) => {
+    res.send([1,2,3]);
+
+});
+
+//Port 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
