729d45a3962c389b819cfcfe87964e118d57b2a9
johnfruscella <johnfruscella@gmail.com>
2020-01-22 18:05:35 -0500

    covered videos 0404 - 0408. Added course and course id route handler

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => {
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+]
+
+app.get('/', (req, res) => {
     res.send(`Welcome!`);
 
 });
 
-app.get('/api/courses', (req,res) => {
-    res.send([1,2,3]);
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
 
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send(`Error 404: The course can not be found`);
+    res.send(course);
 });
 
 //Port 
