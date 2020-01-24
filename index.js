9c96f7c60726927adb58597e88f15fcde5f18929
johnfruscella <johnfruscella@gmail.com>
2020-01-23 20:17:31 -0500

    added posting functionality for courses route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
@@ -17,11 +19,28 @@ app.get('/api/courses', (req, res) => {
 
 });
 
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courese.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send(`Error 404: The course can not be found`);
     res.send(course);
 });
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+})
 
 //Port 
 const port = process.env.PORT || 3000;
